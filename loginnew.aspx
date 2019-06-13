7b14a1e70a5010868259884b4c5a8b24ee4a8d4b
yudhvirharyana <51136900+yudhvirharyana@users.noreply.github.com>
2019-06-13 14:07:35 -0400

    Add files via upload

---
+++ loginnew.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="loginnew.aspx.cs" Inherits="applliedProject.loginnew" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Style.css" rel="stylesheet" />
+    <title></title>
+    <style type="text/css">
+        .loginbox {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    width: 400px;
+    height: 550px;
+    padding: 100px 60px;
+    box-sizing: border-box;
+    background: rgba(0, 0, 0, 0.7);
+}
+        .auto-style1 {
+            font-weight: bold;
+            color: #fff;
+            font-size: large;
+        }
+        .auto-style2 {
+            font-size: x-large;
+        }
+        .auto-style3 {
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="loginbox">
+        <h2 class="auto-style2">PERFUME ONLINE</h2><br/><br/>
+        <form runat="server">
+            <asp:Label text="Username" CssClass="auto-style1" runat="server" />
+            <br />
+            <asp:TextBox runat="server" CssClass="auto-style3" placeholder="enter email" Height="26px" Width="224px" ID="textbox1" />
+            <br />
+            <asp:Label text="Password" CssClass="auto-style1" runat="server" />
+            <br />
+            <asp:TextBox runat="server" CssClass="auto-style3" textmode="Password" placeholder="**********" Height="24px" Width="221px" ID="textbox2" />
+            <strong>
+            <br />
+            <br />
+            <asp:Button text="Login" CssClass="btnlogin" runat="server" Height="39px" Width="97px" OnClick="Unnamed5_Click" style="font-weight: bold; font-size: medium;" /></strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong>
+            <asp:Button text="SignUp" CssClass="btnsignup" runat="server" Height="39px" Width="99px" style="font-weight: bold; font-size: medium;" OnClick="Unnamed6_Click" />
+
+            </strong>
+
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
+
+        </form>
+    </div>
+</body>
+</html>
---
+++ loginnew.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace applliedProject
+{
+    public static class MessageBox
+    {
+        public static void Show(this Page Page, String Message)
+        {
+            Page.ClientScript.RegisterStartupScript(
+               Page.GetType(),
+               "MessageBox",
+               "<script language='javascript'>alert('" + Message + "');</script>"
+            );
+        }
+    }
+    public partial class loginnew : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public string cnstring = "Data Source=desktop-cq119gr;Initial Catalog=perfumeonline;Integrated Security=True";
+
+        protected void Unnamed2_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        protected void Unnamed5_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cnstring);
+            con.Open();
+            if (con.State == System.Data.ConnectionState.Open) ;
+            SqlCommand o = new SqlCommand("select * from signup where username='" + textbox1.Text + "' And password ='" + textbox2.Text + "';", con);
+            //con.Open();
+            SqlDataReader r = o.ExecuteReader();
+            if(r.Read())
+            {
+                Response.Redirect("~/home.aspx");
+            }
+            else
+            {
+                MessageBox.Show(this, "Username and Password Is Not Correct");
+            }
+
+            //string query1 = "select * from Login where username='" + textbox1.Text + "' and password='" + textbox2.Text + "'";
+            //SqlCommand cmd = new SqlCommand(query1, con);
+            ////cmd.ExecuteNonQuery();
+            //string a = textbox1.Text;
+            //string b = textbox2.Text;
+
+           /** if(textbox1.Text.Length==0 && textbox2.Text.Length==0)
+            {
+                MessageBox.Show(this,"Please Enter Username And Password");
+                //ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert(' hhhh ');", true); popup 1
+
+                //Response.Write("<script language=javascript>alert('Please enter username and Password.')</script>"); popup2
+            }
+            else
+            {
+     
+                if(textbox1.Text.Trim() == a  && textbox2.Text.Trim() == b)
+                {
+                    Response.Redirect("~/WebForm1.aspx");
+                }
+                else
+                {
+                    MessageBox.Show(this, "Username Password Is Not Correct");
+                }
+            } **/
+        }
+
+        protected void Unnamed6_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Feedback.aspx");
+        }
+    }
+}
